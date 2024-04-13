53a4be2c20a60414a74cc4f43c63da641a0d84ac
Henry Leung <henryleungj@gmail.com>
2024-04-13 05:19:34 -0700

    switched api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 
 const app = express();
@@ -22,13 +25,27 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+// + symbol.toUpperCase() + ("-USD" || "-USDC" || "-USDT")
+
+function round(num) {
+    return Math.round(num * 100) / 100;
+}
 
 app.post("/", async (req, res) => {
     try {
-        const symbol = req.body.symbol;
-        const response = await axios.get("https://api.blockchain.com/v3/exchange/tickers/" + symbol.toUpperCase() + ("-USD" || "-USDC" || "-USDT"));
-        res.render("index.ejs", {data: response.data});
-        console.log(response.data);
+        const symbol = req.body.symbol.toUpperCase();
+        const response = await axios.get("https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=" + symbol,
+        {headers: {
+            "X-CMC_PRO_API_KEY": process.env.API,
+        }}
+    );
+        let price = response.data.data[Object.keys(response.data.data)[0]][0].quote.USD.price;
+
+        if (Math.round(price) > 0) {
+            price = round(price);
+        }
+        res.render("index.ejs", {data: {price: price, symbol: response.data.data[Object.keys(response.data.data)[0]][0].symbol, volume_24h: round(response.data.data[Object.keys(response.data.data)[0]][0].quote.USD.volume_change_24h), price_24h: round(response.data.data[Object.keys(response.data.data)[0]][0].quote.USD.percent_change_1h)}});
+        console.log(response.data.data.BTC[0].symbol);
     } catch (error) {
         res.render("index.ejs", {error: {}});
     }
